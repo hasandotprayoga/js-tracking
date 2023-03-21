3d392f49fee52d0fa748e7b61a7d581fed4fca2c
Hasan Prayoga <hasandotprayoga@gmail.com>
2023-03-21 10:55:06 +0700

    feat: 🎸 add custom sscript

---
+++ custom-script.js
@@ -0,0 +1,40 @@
+function stringToNodes(stribg) {
+  const nodes = [];
+  new DOMParser()
+    .parseFromString(stribg, "text/html")
+    .head.childNodes.forEach((i) => {
+      if (i.tagName === "SCRIPT") {
+        nodes.push(i);
+      }
+    });
+  return nodes;
+}
+
+function nodeToElement(node) {
+  const el = document.createElement("script");
+  node.getAttributeNames().forEach((name) => {
+    el.setAttribute(name, node.getAttribute(name));
+  });
+
+  el.textContent = node.textContent;
+  return el;
+}
+
+function appendElement(string, destination = "head") {
+  stringToNodes(string).forEach((node) => {
+    const el = nodeToElement(node);
+
+    document[destination].appendChild(el);
+  });
+}
+
+() => {
+  window.cs = ({ headerCode, footerCode }) => {
+    if (typeof window !== "undefined") {
+      try {
+        appendElement(headerCode, "head");
+        appendElement(footerCode, "body");
+      } catch (error) {}
+    }
+  };
+};
